b699a6c4fae09529a08458685a83242470396646
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-02-27 14:12:01 -0800

    Update main.js

--- scripts/game/main.js
+++ scripts/game/main.js
@@ -1,6 +1,7 @@
 const score = 0;
+document.getElementById("scoreCounter").innerHTML = score;
 
 function click() {
     score += 1;
-    document.getElementById("scoreCounter").innerHTML = score
+    document.getElementById("scoreCounter").innerHTML = score;
 }
