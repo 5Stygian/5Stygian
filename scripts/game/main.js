60b8ec9b3671003a7daa2e9e5e0048f8d67b33dc
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-04-15 15:38:18 -0700

    Update main.js

--- scripts/game/main.js
+++ scripts/game/main.js
@@ -1,9 +1,12 @@
+// Set elements
 let score = 0;
 document.getElementById("scoreCounter").innerHTML = score;
 
+// INCREMENTER
 function increment() {
     score += 1;
     document.getElementById("scoreCounter").innerHTML = score;
+    console.log("")
 }
 
 document.getElementById("incrementer").addEventListener('click', increment);
