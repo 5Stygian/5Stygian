b7eef233f990d34255520b2ee255b8d794d25d8e
Henry Graff <rlcrafistohard@gmail.com>
2025-04-16 16:56:30 -0700

    blazingly fast

--- scripts/game/main.js
+++ scripts/game/main.js
@@ -1,12 +1,14 @@
 // Set elements
+let incrementAmount = 1;
 let score = 0;
 document.getElementById("scoreCounter").innerHTML = score;
 
 // INCREMENTER
 function increment() {
-    score += 1;
+    manualIncrementLog = "Increment score manually"
+    score += incrementAmount;
     document.getElementById("scoreCounter").innerHTML = score;
-    console.log("")
+    console.log(manualIncrementLog)
 }
 
 document.getElementById("incrementer").addEventListener('click', increment);
