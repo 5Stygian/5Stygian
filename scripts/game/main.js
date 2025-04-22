cd796a71d2fe2741182b0f6b917af0c749dd79e3
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-04-22 10:57:36 -0700

    Update main.js

--- scripts/game/main.js
+++ scripts/game/main.js
@@ -5,10 +5,15 @@ document.getElementById("scoreCounter").innerHTML = score;
 
 // INCREMENTER
 function increment() {
-    manualIncrementLog = "Increment score manually"
+    let manualIncrementLog = "Increment score manually";
     score += incrementAmount;
     document.getElementById("scoreCounter").innerHTML = score;
     console.log(manualIncrementLog)
 }
 
 document.getElementById("incrementer").addEventListener('click', increment);
+
+// UPGRADES
+function upgradeIncrementer() {
+    incrementAmount += 1;
+}
