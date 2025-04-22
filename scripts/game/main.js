a92504aed83b91aa6cd975321105a5410a5cd66b
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-04-22 15:46:39 -0700

    sifma

--- scripts/game/main.js
+++ scripts/game/main.js
@@ -1,31 +1,12 @@
-/*========================GARBLED NONSESE=======================================================================================
-After thoroughly reading through this file, any form of the word "increment" may loose meaning for a random amount of time.
-This warning is not to be ignored.
-==============================================================================================================================*/
-
-// Set variables
-let score = 0;
-let incrementAmount = 1;
-let incrementIncrementerCost = 25;
-let upgradeCostScaling = 1.15;
-
 // Set elements
+let score = 0;
 document.getElementById("scoreCounter").innerHTML = score;
 
 // INCREMENTER
 function increment() {
-    let manualIncrementLog = "Increment score manually";
-    score += incrementAmount;
+    score += 1;
     document.getElementById("scoreCounter").innerHTML = score;
-    console.log(manualIncrementLog)
+    console.log("")
 }
-document.getElementById("incrementer").addEventListener('click', increment);
 
-// UPGRADES
-function incrementIncrementer() {
-    if (score >= upgradeIncementerCost) {
-        incrementIncementerCost *= upgradeCostScaling;
-        incrementAmount += 1;
-    }
-}
-document.getElementById("incrementIncrementer").addEventListener('click', incrementIncrementer);
+document.getElementById("incrementer").addEventListener('click', increment);
