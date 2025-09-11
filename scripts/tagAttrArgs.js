ec51b00ee46cfb8172bd3b96089f08aeea130b40
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-09-11 09:50:18 -0700

    Create tagAttrArgs.js

---
+++ scripts/tagAttrArgs.js
@@ -0,0 +1,7 @@
+// logic for getting attributes from html tags and setting css properties
+
+const Element.getAttribute() = getAttribute();
+
+const foo = async () => {
+  console.log("unimplemented");
+}
