25fcea80c239a6faa78bf6dbca006ad2ab717968
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-09-11 10:37:26 -0700

    write setBgImage function | t1

--- scripts/tagAttrArgs.js
+++ scripts/tagAttrArgs.js
@@ -1,7 +1,12 @@
 // logic for getting attributes from html tags and setting css properties
 
-const Element.getAttribute() = getAttribute();
-
-const foo = async () => {
-  console.log("unimplemented");
+// sets the background image 
+const setBgImage = async () => {
+  const selectedTags = document.querySelectorAll("#taa_img");
+  console.log(selectedTags);
+  for (let i=0; i < selectedTags.length; i++) {
+    selectedTags[i].style.backgroundImage = selectedTags[i].getAttribute("data-bgImg");
+  }
 }
+
+setBgImage()
