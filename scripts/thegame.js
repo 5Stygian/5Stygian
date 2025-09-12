2d171b54c0c566041ace980dfb9f33a4c05f9ce2
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-09-12 01:06:07 -0700

    Update thegame.js

--- scripts/thegame.js
+++ scripts/thegame.js
@@ -1,6 +1,6 @@
 const theReminder = () => {
-  let rndInt = Math.floor(Math.random() * 1000001);
-  if (rndInt === 555555) { // 1/1000000 chance
+  let rndInt = Math.floor(Math.random() * 2);
+  if (rndInt === 2) { // 1/1000000 chance
     window.alert("You lost the game!\nThis is a 1 in 1 million chance\nCheck /scripts/thegame.js for confirmation");
     console.log(`You rolled a ${rndInt}!\n\nYou lost the game, but you already knew that`);
   } else {
