d2744dc5124bf0c5ddc462d27e9ba8075b414ce9
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-09-12 01:05:22 -0700

    rewrite

--- scripts/thegame.js
+++ scripts/thegame.js
@@ -1,11 +1,9 @@
-function getRndInteger() {
-  return Math.floor(Math.random() * 1000001); // Generates 0-1 million
-}
-
-function theReminder() {
-  let rndInt = getRndInteger();
-  console.log(rndInt);
+const theReminder = () => {
+  let rndInt = Math.floor(Math.random() * 1000001);
   if (rndInt === 555555) { // 1/1000000 chance
     window.alert("You lost the game!\nThis is a 1 in 1 million chance\nCheck /scripts/thegame.js for confirmation");
+    console.log(`You rolled a ${rndInt}!\n\nYou lost the game, but you already knew that`);
+  } else {
+    console.log(`You rolled a ${rndInt}!\n\nYou won the game, or have you?`);
   }
 }
