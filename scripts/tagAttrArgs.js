6811db1cd7c35e3031dd3092adbabc2f031b3684
5Stygian <rlcrafistohard@gmail.com>
2025-09-12 22:22:36 -0700

    add more styling to scbal and expand tagAttrArgs.js

--- scripts/tagAttrArgs.js
+++ scripts/tagAttrArgs.js
@@ -1,18 +1,44 @@
 // logic for getting attributes from html tags and setting css properties
 
-// sets the background image 
-const setBgImage = async () => {
-  const selectedTags = document.querySelectorAll("#taa_img");
-  console.log(selectedTags);
-  for (let i=0; i < selectedTags.length; i++) {
-    let bgImg = selectedTags[i].getAttribute("data-bgimg");
-    selectedTags[i].style.backgroundImage = `url(${bgImg})`;
+const tags = [
+  "#taa_clr"
+];
+
+const attr = [
+  "data-clr"
+];
+
+const property = [
+  "color"
+];
+
+// sets the style of an element based on attributes
+const setAttrStyles = async () => {
+  for (let i = 0; i < tags.length; i++) {
+    const selectedTags = document.querySelectorAll(`${tags[i]}`);
+    console.log(selectedTags);
+    for (let j = 0; j < selectedTags.length; j++) {
+      let css = selectedTags[j].getAttribute(`${attr[j]}`);
+      selectedTags[j].style[property[j]] = css;
+    }
   }
 }
 
-// execs all functions one at a time
-const setAttrs = async () => {
-  await setBgImage();
+// sets the background image
+const setBgImage = async () => {
+  const selectedTags = document.querySelectorAll(`#taa_img`);
+  console.log(selectedTags);
+  for (let j = 0; j < selectedTags.length; j++) {
+    let css = selectedTags[j].getAttribute(`data-bgimg`);
+    selectedTags[j].style.backgroundImage = `url(${css})`;
+  }
 }
 
-setAttrs();
+// calls all functions. idk why that needs a whole function but it js makes sense to me
+const start = async () => {
+  console.log(`${tags}, ${attr}, ${property}`);
+  setAttrStyles();
+  setBgImage();
+}
+
+start();
