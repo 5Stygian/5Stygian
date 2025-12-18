f7c46a9106e1c7e773ca8716c0bf33093a76d46b
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-18 12:29:12 -0800

    remove ?

--- src/app/components/layout/content.tsx
+++ src/app/components/layout/content.tsx
@@ -9,7 +9,7 @@ interface ContentProps {
   subHeader?: string;
 }
 
-export default function Content({ children, domHeader, subHeader? }: ContentProps) {
+export default function Content({ children, domHeader, subHeader }: ContentProps) {
   return (
     <div className="w-2/3 h-full bg-slate-800 shadow-md shadow-slate-800">
       <DomHeader text={domHeader} />
