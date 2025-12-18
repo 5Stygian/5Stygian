3e5eab7a3097b198773fd2e8441b09ee6e7fc45c
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-18 12:19:51 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import * as c from './compnents/index';
+
 export default function Home() {
   return (
     <main className="w-screen h-full bg-zinc-950">
-      <h1 className="text-7xl font-bold">Jello, world!</h1>
+      <c.Content domHeader="Index" subHeader="o algo"></c.Content>
     </main>
   );
 }
