04c879415ef7452351a20dc7f68cf31fb35e66c0
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-18 14:09:50 -0800

    change background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import * as c from './components/index';
 
 export default function Home() {
   return (
-    <main className="w-screen h-full bg-zinc-950">
+    <main className="w-screen h-full bg-zinc-800">
       <c.Content domHeader="Index" subHeader="o algo">
         children
       </c.Content>
