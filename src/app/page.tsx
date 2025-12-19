675d9996ecef96fdd4c410ff338c1dc4d58e1072
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-19 10:44:23 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Index() {
   return (
     <c.Content domHeader="Index" subHeader="o algo">
-      <Link href="/pages/components">Components</Link>
+      <Link href="/papers/components">Components</Link>
     </c.Content>
   );
 }
