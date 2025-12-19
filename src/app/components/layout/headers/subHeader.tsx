04fd8444c8c144a525da263ff46279c6bb0594bc
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-19 10:24:51 -0800

    Update subHeader.tsx

--- src/app/components/layout/headers/subHeader.tsx
+++ src/app/components/layout/headers/subHeader.tsx
@@ -1,3 +1,8 @@
+/** It stands for "submissive header".
+  * It's a reference to what is was called in the original named repo.
+  */
+  
+
 interface SubHeaderProps {
   text: string | undefined;
 }
