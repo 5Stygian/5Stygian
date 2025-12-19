9226b208b6bbdcebb4e6b1d1d00a5ea8f2bb9a8c
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-19 09:03:25 -0800

    draft post

--- src/app/components/post/post.tsx
+++ src/app/components/post/post.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 interface PostProps {
   children: React.ReactNode;
+  tags: string[];
 }
 
-export default function Post({ children }: PostProps) {
+export default function Post({ children, tags }: PostProps) {
   return (
-    <div>
+    <div className="w-3/4 h-fit my-8 mx-auto py-6 px-4 bg-gray-800 border-2 border-gray-700">
+      <div>
+        <span className="text-xl font-bold">Tags</span>
+        <hr className="w-7/8 my-2" />
+        {tags}
+      </div>
       {children}
     </div>
   );
