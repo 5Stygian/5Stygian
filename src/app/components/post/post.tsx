90448444d66cd36288be8fbff4df03f26600c8c9
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-19 10:12:02 -0800

    separate tags by a comma

--- src/app/components/post/post.tsx
+++ src/app/components/post/post.tsx
@@ -11,14 +11,20 @@ interface PostProps {
 export default function Post({ children, title, tags }: PostProps) {
   const [postTitle, setPostTitle] = useState<string>(title);
   const [postTags, setPostTags] = useState<string[]>(tags);
+
+  let displayPostTags: string;
+
+  if (Array.isArray(postTags)) {
+   displayPostTags = postTags.join(", ");
+  }
   
   return (
     <div className="w-3/4 h-fit my-8 mx-auto py-6 px-4 bg-gray-800 border-2 border-gray-700">
-      <span className="block text-3xl font-bold ">{title}</span>
+      <span className="block text-3xl font-bold ">{postTitle}</span>
       <div className="my-4">
         <span className="text-xl font-bold">Tags</span>
         <hr className="w-7/8 my-2" />
-        <div>{tags}</div>
+        <div>{displayPostTags}</div>
       </div>
       <span className="block text-3xl font-bold">Summary</span>
       {children}
