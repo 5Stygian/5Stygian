dc09410ccbe9f71466d09701728982d302bfbda8
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-19 10:23:58 -0800

    Update domHeader.tsx

--- src/app/components/layout/headers/domHeader.tsx
+++ src/app/components/layout/headers/domHeader.tsx
@@ -1,4 +1,7 @@
-/** not to be confused with Document Object Model Header */
+/** not to be confused with Document Object Model Header.
+  * It stands for "dominant header". It's a reference to what it was called
+  * in the original named repo.
+  */
 
 interface DomHeaderProps {
   text: string;
