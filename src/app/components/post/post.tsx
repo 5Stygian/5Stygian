2d828da11982cf7129e28c393b5d1fb6c38307c9
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-19 10:28:57 -0800

    Update post.tsx

--- src/app/components/post/post.tsx
+++ src/app/components/post/post.tsx
@@ -21,10 +21,12 @@ export default function Post({ children, title, tags }: PostProps) {
   return (
     <div className="w-3/4 h-fit my-8 mx-auto py-6 px-4 bg-gray-800 border-2 border-gray-700">
       <span className="block text-3xl font-bold ">{postTitle}</span>
+      <hr className="w-full h-2 bg-gray-700 border-none" />
       <div className="my-4">
         <span className="text-xl font-bold">Tags</span>
-        <div className="mt-4">{displayPostTags}</div>
+        <div className="mt-3">{displayPostTags}</div>
       </div>
+      <hr className="w-full h-2 bg-gray-700 border-none" />
       <span className="block text-3xl font-bold">Summary</span>
       {children}
     </div>
