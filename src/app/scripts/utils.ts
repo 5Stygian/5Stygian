299ef4f634256e7ef59c524a15c6a0a4032392df
5Stygian <rlcrafistohard@gmail.com>
2026-06-30 20:26:01 -0700

    reword jsdoc

--- src/app/scripts/utils.ts
+++ src/app/scripts/utils.ts
@@ -1,15 +1,15 @@
 /**
  * Capitalizes the word that is passed. Will throw an error if the first character isn't a letter.
  *
- * This function only works with letters in the ASCII table.
+ * This function only works with ASCII characters.
  */
 export function capitalize(text: string): string {
   let result: string = "";
   const firstLetterASCII: number = text.charCodeAt(0);
 
   if (
-    firstLetterASCII < 97 ||
-    (firstLetterASCII > 122 && !isNaN(firstLetterASCII))
+    (firstLetterASCII < 97 || firstLetterASCII > 122) &&
+    !isNaN(firstLetterASCII)
   ) {
     result = "THATS NOT A LOWER CASE LETTER DUMBASS";
     new Error("THATS NOT A LOWER CASE LETTER DUMBASS");
