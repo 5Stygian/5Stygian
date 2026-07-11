634de3494c1b468cdffdb5d534d142cf54280851
5Stygian <rlcrafistohard@gmail.com>
2026-07-11 15:52:04 -0700

    make the table of contents collapsible

--- src/app/components/toc/toc.tsx
+++ src/app/components/toc/toc.tsx
@@ -9,6 +9,11 @@ export default function TableOfContents() {
 
   useEffect(() => {
     const headerIds: Array<string> = [];
+    
+    const collapse: HTMLSpanElement = document.getElementById("toc-collapse") as HTMLSpanElement;
+    const divider: HTMLHRElement = document.getElementById("toc-divider") as HTMLHRElement;
+    const contents: HTMLDivElement = document.getElementById("toc-contents") as HTMLDivElement;
+    let contentsAreCollapsed: boolean = false
 
     for (let i = 1; i <= 6; i++) {
       const headers = document.getElementsByTagName(`h${i}`);
@@ -19,22 +24,42 @@ export default function TableOfContents() {
     }
 
     setIds(headerIds);
-  }, []);
 
+    collapse.addEventListener("click", (): void => {
+      contentsAreCollapsed = !contentsAreCollapsed;
+
+      contentsAreCollapsed ?
+        contents.className = "hidden" :
+        contents.className = "";
+    });
+  }, []);
+    
   return (
     <div className="fixed top-0 right-0 mt-4 mr-4 p-2.5 bg-zinc-900 border-2 border-zinc-700 z-9001">
-      <strong>Table of Contents</strong>
-      <hr className="my-2" />
-      {ids.map((id, index) => {
-        const idNameArray: Array<string> = id.split("-");
-        let displayName: string = idNameArray.map(capitalize).join(" ");
+      <div>
+        <strong>Table of Contents</strong>
+        <span
+          id="toc-collapse"
+          className="ml-2 pl-2 font-extrabold border-l-2 border-l-zinc-500/35 select-none hover:cursor-pointer"
+        >
+          O
+        </span>
+      </div>
+      <div id="toc-contents">
+        <hr id="toc-divider" className="my-2" />
+        {ids.map((id, index) => {
+          const idNameArray: Array<string> = id.split("-");
+          let displayName: string = idNameArray
+            .map(capitalize)
+            .join(" ");
 
-        return (
-          <div key={index}>
-            <Link href={`#${id}`}>{displayName}</Link>
-          </div>
-        );
-      })}
+          return (
+            <div key={index}>
+              <Link href={`#${id}`}>{displayName}</Link>
+            </div>
+          );
+        })}  
+      </div>
     </div>
   );
 }
