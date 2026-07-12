0d2bac3fea6454b3f9799301e9a27a87d7f9a2a7
5Stygian <rlcrafistohard@gmail.com>
2026-07-11 20:29:40 -0700

    make shit slightly brighter

--- src/app/components/layout/topbar.tsx
+++ src/app/components/layout/topbar.tsx
@@ -4,7 +4,7 @@ const LinkStyle: string = "text-white! cursor-pointer";
 
 export default function TopBar() {
   return (
-    <nav className="fixed w-full pl-2 sm:pl-6 bg-zinc-900 border-b-4 border-b-zinc-500/35 z-9000">
+    <nav className="fixed w-full pl-2 sm:pl-6 bg-zinc-800 border-b-4 border-b-zinc-500/35 z-9000">
       <Link href="/" className={LinkStyle}>
         <h1 className="inline pr-6 italic border-r-4 border-r-zinc-500/35">
           5Stygian
