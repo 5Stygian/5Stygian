185dedbc139c38c2485714e2ff352ecf6018a6b7
5Stygian <rlcrafistohard@gmail.com>
2026-07-14 00:48:33 -0700

    add an egg on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import * as c from "./components/index";
 
 export default function Index() {
   return (
-    <c.Content domHeader="Index" subHeader="hi )))">
+    <c.Content domHeader="Index" subHeader="hi )))" egg="why are you looking here lol">
       hello
     </c.Content>
   );
