075aa0455a8591d793499e12788e6e51bc48e58d
5Stygian <rlcrafistohard@gmail.com>
2026-07-14 00:38:21 -0700

    webring stuff

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -4,5 +4,6 @@ import List from "./misc/list";
 import Status from "./misc/status";
 import Post from "./post/post";
 import TableOfContents from "./toc/toc";
+import Webrings from "./webrings/webrings";
 
-export { Content, Notice, List, Post, Status, TableOfContents };
+export { Content, Notice, List, Post, Status, TableOfContents, Webrings };
--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,3 +1,8 @@
-export const WBP1Name: string = "World Building Project 1";
+export const WBP1Name: string =
+  "World Building Project 1";
+
 export const TopBarLinkStyle: string
   = "block py-0.5 pl-8 text-[18px] font-bold text-white! hover:cursor-pointer";
+  
+export const WebringLinkStyle: string =
+  "hover:filter-none!";
